31f5ca8d748aa52138d12366ec25848ac9c285a1
Julius Weber <38101391+ciearius@users.noreply.github.com>
2022-11-02 22:19:22 +0100

    simplify api

--- decode.go
+++ decode.go
@@ -4,7 +4,6 @@ import (
 	"errors"
 	"fmt"
 
-	"github.com/cearius/go-charmagic/pkg/matching"
 	"golang.org/x/text/encoding"
 	"golang.org/x/text/encoding/ianaindex"
 )
@@ -12,81 +11,56 @@ import (
 var errNoMatch = errors.New("no match in index")
 var errNotSupported = errors.New("not supported")
 
-// TODO: refactor and define clear interface
+// MustGetDecoder tries to get a matching encoding or returns nil
+func GetDecoder(name string) (encoding.Encoding, error) {
+	enc, err := query_all_indexes(name)
 
-func GetDecoder(name string) (enc encoding.Encoding, err error) {
-	enc, err = ianaindex.IANA.Encoding(name)
-
-	if err == nil {
-		return
+	if err != nil {
+		return nil, fmt.Errorf("no encoder found for %s: %s", name, err)
 	}
 
-	enc, err = ianaindex.MIME.Encoding(name)
-
-	if err == nil {
-		return
-	}
-
-	enc, err = ianaindex.MIB.Encoding(name)
-
-	if err == nil {
-		return
-	}
-
-	return nil, fmt.Errorf("no encoder found for %s", name)
+	return enc, nil
 }
 
-func GetDecoderFromResult(r *matching.Result) (enc encoding.Encoding) {
-	name := r.Charset
+// MustGetDecoder tries to get a matching encoding or panics.
+//
+// Use discouraged since errors should always be checked.
+func MustGetDecoder(name string) encoding.Encoding {
+	enc, err := query_all_indexes(name)
 
-	// ianaindex.IANA, ianaindex.MIME, ianaindex.MIB,
-
-	enc = get_encoding(ianaindex.IANA, name)
-
-	if enc != nil {
-		return
-	}
-
-	enc = get_encoding(ianaindex.MIME, name)
-
-	if enc != nil {
-		return
-	}
-
-	enc = get_encoding(ianaindex.MIB, name)
-
-	if enc == nil {
-		panic(fmt.Errorf("no encoder found for '%s'", name))
+	if err != nil {
+		panic(err)
 	}
 
 	return enc
 }
 
-// get_encoding tries to fetch the encoding matching the name from the provided Index
-func get_encoding(idx *ianaindex.Index, name string) encoding.Encoding {
-	enc, err := queryIndex(idx, name)
+// query_all_indexes looks up a name in all three known encoding indexes and returns the first non-nil encoding.
+// In case no non-nil encoding is found an error is returned
+func query_all_indexes(name string) (encoding.Encoding, error) {
+	var enc encoding.Encoding
+	var err error
+
+	// TODO: #7 make sure there are no weird edgecases in encoding indexes
+
+	enc, err = get_encoding(ianaindex.IANA, name)
 
 	if enc != nil {
-		return enc
+		return enc, err
 	}
 
-	if err == errNotSupported {
-		panic(fmt.Errorf("encoding '%s' not supported", name))
+	enc, err = get_encoding(ianaindex.MIME, name)
+
+	if enc != nil {
+		return enc, err
 	}
 
-	if err == errNoMatch {
-		return nil
-	}
-
-	return enc
+	return get_encoding(ianaindex.MIB, name)
 }
 
-// queryIndex checks the index and returns an encoding and a bool.
-func queryIndex(idx *ianaindex.Index, name string) (enc encoding.Encoding, err error) {
-
-	// enc == nil && err != nil => no match
-	// enc == nil && err == nil => not supported
-	enc, err = idx.Encoding(name)
+// get_encoding looks up a name in one specific index and checks for the two known errors.
+func get_encoding(idx *ianaindex.Index, name string) (encoding.Encoding, error) {
+	enc, err := idx.Encoding(name)
 
 	if enc == nil && err != nil {
 		return nil, errNoMatch
@@ -96,5 +70,5 @@ func queryIndex(idx *ianaindex.Index, name string) (enc encoding.Encoding, err e
 		return nil, errNotSupported
 	}
 
-	return
+	return enc, nil
 }
